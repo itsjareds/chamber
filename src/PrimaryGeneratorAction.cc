164aac12012b3004128411fc5bb48912a565875d
Mark Schott <reudc13@g.clemson.edu>
2014-06-20 15:40:53 -0400

    more stuff uncommented

--- src/PrimaryGeneratorAction.cc
+++ src/PrimaryGeneratorAction.cc
@@ -44,6 +44,8 @@
 #include "G4SystemOfUnits.hh"
 #include "Randomize.hh"
 
+#include <cmath>
+
 //....oooOO0OOooo........oooOO0OOooo........oooOO0OOooo........oooOO0OOooo......
 
 PrimaryGeneratorAction::PrimaryGeneratorAction()
@@ -90,157 +92,6 @@ void PrimaryGeneratorAction::GeneratePrimaries(G4Event* anEvent)
 sources= [[0]*4 for _ in [0]*30]
 
   	
-sources[0][0] = 14.3	
-sources[0][1] = 52.5	
-sources[0][2] = 407.4	
-sources[0][3] = 103.8	
-  	
-sources[1][0] = 15.6	
-sources[1][1] = 43.75	
-sources[1][2] = 405.5	
-sources[1][3] = 113.2	
-  	
-sources[2][0] = 16.9	
-sources[2][1] = 35.	
-sources[2][2] = 403.5	
-sources[2][3] = 122.6	
-  	
-sources[3][0] = 18.2	
-sources[3][1] = 26.25	
-sources[3][2] = 401.3	
-sources[3][3] = 131.9	
-  	
-sources[4][0] = 19.5	
-sources[4][1] = 17.5	
-sources[4][2] = 398.8	
-sources[4][3] = 141.2	
-  	
-sources[5][0] = 20.8	
-sources[5][1] = 8.75	
-sources[5][2] = 396.1	
-sources[5][3] = 150.5	
- 	
-sources[6][0] = 22.1	
-sources[6][1] = 0.	
-sources[6][2] = 393.2	
-sources[6][3] = 159.7	
-  	
-sources[7][0] = 23.4	
-sources[7][1] = 52.5	
-sources[7][2] = 390.	
-sources[7][3] = 168.8	
-
-sources[8][0] = 24.7	
-sources[8][1] = 42.	
-sources[8][2] = 386.7	
-sources[8][3] = 177.9	
-  	
-sources[9][0] = 26.	
-sources[9][1] = 31.5	
-sources[9][2] = 383.1	
-sources[9][3] = 186.9	
-  	
-sources[10][0] = 27.3	
-sources[10][1] = 21.	
-sources[10][2] = 379.4	
-sources[10][3] = 195.8	
-
-
-sources[11][0] = 28.6	
-sources[11][1] = 10.5	
-sources[11][2] = 375.4	
-sources[11][3] = 204.7	
-  	  	
-sources[12][0] = 29.9	
-sources[12][1] = 0.	
-sources[12][2] = 371.2	
-sources[12][3] = 213.4
-  	
-sources[13][0] = 31.2	
-sources[13][1] = 52.5	
-sources[13][2] = 366.7	
-sources[13][3] = 222.1	
-  	
-sources[14][0] = 32.5	
-sources[14][1] = 42.	
-sources[14][2] = 362.1	
-sources[14][3] = 230.7	
-  	
-sources[15][0] = 33.8	
-sources[15][1] = 31.5	
-sources[15][2] = 357.4	
-sources[15][3] = 239.2	
- 	
-sources[16][0] = 35.1	
-sources[16][1] = 21.	
-sources[16][2] = 352.2	
-sources[16][3] = 247.6	
-  	
-sources[17][0] = 36.4	
-sources[17][1] = 10.5	
-sources[17][2] = 347.	
-sources[17][3] = 255.8	
-  	
-sources[18][0] = 37.7	
-sources[18][1] = 0.	
-sources[18][2] = 341.5	
-sources[18][3] = 264.	
-  	  	
-sources[19][0] = 39.	
-sources[19][1] = 52.5	
-sources[19][2] = 335.9	
-sources[19][3] = 272.	
-  	  	
-sources[20][0] = 40.3	
-sources[20][1] = 42.	
-sources[20][2] = 330.1	
-sources[20][3] = 279.9	
- 	  	
-sources[21][0] = 41.6	
-sources[21][1] = 31.5	
-sources[21][2] = 324.	
-sources[21][3] = 287.7	
-  	  	
-sources[22][0] = 42.9	
-sources[22][1] = 21.	
-sources[22][2] = 317.8	
-sources[22][3] = 295.3	
-  	  	
-sources[23][0] = 44.2	
-sources[23][1] = 10.5	
-sources[23][2] = 311.4	
-sources[23][3] = 302.8	
-  	  	
-sources[24][0] = 45.5	
-sources[24][1] = 0.	
-sources[24][2] = 304.8	
-sources[24][3] = 310.2	
-  	  
-sources[25][0] = 46.8	
-sources[25][1] = 52.	
-sources[25][2] = 298.	
-sources[25][3] = 317.4	
-  	  	
-sources[26][0] = 48.1	
-sources[26][1] = 40.	
-sources[26][2] = 291.1	
-sources[26][3] = 324.4	
-  	  	
-sources[27][0] = 49.4	
-sources[27][1] = 28.	
-sources[27][2] = 283.9	
-sources[27][3] = 331.3	
-	  	  	
-sources[28][0] = 50.7	
-sources[28][1] = 16.	
-sources[28][2] = 276.6	
-sources[28][3] = 338.	
-  	  	
-sources[29][0] = 52.	# T
-sources[29][1] = 4.	    # Q
-sources[29][2] = 269.2	# R
-sources[29][3] = 344.5	# Z+30
-#source coordinates end
 
 
 import sys
@@ -290,28 +141,28 @@ except AttributeError:
 
 def set_source(new_point, rotationX, rotationZ, thetaDeg, gunAxis):
 	"""set up the source and collimators for a given angle and hypocenter"""
-	theta=thetaDeg*3.14159265/180.0
+	G4double theta=thetaDeg*3.14159265/180.0;
 	new_point=Vector(new_point)
 
 	gen=mainVars.myGenerator
 	mainVars.myGenerator.SetBeamSize(2.5908/2)    #smallestTubeInnerDiam=2.5908
 	mainVars.myGenerator.SetAngularHalfSpread(math.pi) #distribute beam over whole target
 
-#	coords for Gun position
-	containerHeight = 50.0126 + 51.5*2 + 89 + 1.5
-	largestTubeHeight = 50.0126
-	largestTubePosZ=largestTubeHeight/2 - containerHeight/2
-	smallestTubeHeight = 32.9438 * mm
-	smallestBackCapHeight = 2 * mm
-	smallestCapHeight = 0.685 * mm
-	cobaltHeight = smallestTubeHeight - smallestBackCapHeight - smallestCapHeight
+//#	coords for Gun position
+	G4double containerHeight = 50.0126 + 51.5*2 + 89 + 1.5;
+	G4double largestTubeHeight = 50.0126;
+	G4double largestTubePosZ=largestTubeHeight/2 - containerHeight/2;
+	G4double smallestTubeHeight = 32.9438 * mm;
+	G4double smallestBackCapHeight = 2 * mm;
+	G4double smallestCapHeight = 0.685 * mm;
+	G4double cobaltHeight = smallestTubeHeight - smallestBackCapHeight - smallestCapHeight;
 	largestTubeHeight = 50.0126 * mm
 	smallestTubePosZ = ((largestTubeHeight/mm-smallestTubeHeight/mm)/2-1.4224 -0.4064 )*mm
 	cobaltPosZ = (smallestTubePosZ/mm+(smallestTubeHeight/mm-cobaltHeight/mm)/2-smallestCapHeight/mm)*mm 
 	insideFillingX = 0.0 
 	insideFillingY = 0.0 
 	fillingPosZ= cobaltPosZ+largestTubeHeight/2 - containerHeight/2
-	print new_point,rotationX,rotationZ,theta
+	//print new_point,rotationX,rotationZ,theta
 	#lenti kettot meg kell rotalni thetaval, kulonben a forrasok a helyukon maradnak, csak a vas megy
 	rotgunAxis=gunAxis.rotate(theta,0,0)
 	rotnew_point=new_point.rotate(theta,0,0)
@@ -321,10 +172,10 @@ def set_source(new_point, rotationX, rotationZ, thetaDeg, gunAxis):
 	mainVars.detectorInfo.containerPhys.relocate( pos=new_point+containerHeight/2*rotgunAxis, rot=rotationX*rotationZ*Matrix(RotationZ(theta)))
 	mainVars.detectorInfo.plexiPhys.relocate( pos=new_point+(containerHeight+5)*rotgunAxis, rot=rotationX*rotationZ*Matrix(RotationZ(theta)))  # ez nem biztos, hogy a helyen van
 
-	#kozepe trafo
-	#gen.SetGunPosition(rotnew_point+fillingPosZ*rotgunAxis)  #
-	#mainVars.detectorInfo.containerPhys.relocate( pos=new_point, rot=rotationX*rotationZ*Matrix(RotationZ(theta)))
-	#mainVars.detectorInfo.plexiPhys.relocate( pos=new_point+(28.5+89+5)*gunAxis, rot=rotationX*rotationZ*Matrix(RotationZ(theta)))  # ez nem biztos, hogy a helyen van
+	//#kozepe trafo
+	//#gen.SetGunPosition(rotnew_point+fillingPosZ*rotgunAxis)  #
+	//#mainVars.detectorInfo.containerPhys.relocate( pos=new_point, rot=rotationX*rotationZ*Matrix(RotationZ(theta)))
+	//#mainVars.detectorInfo.plexiPhys.relocate( pos=new_point+(28.5+89+5)*gunAxis, rot=rotationX*rotationZ*Matrix(RotationZ(theta)))  # ez nem biztos, hogy a helyen van
 
 
 
@@ -427,31 +278,184 @@ if nIons>0:
 */
 void runsweep(G4int n_events, G4double thetaDeg, G4double deltatheta)
 {
+	std::vector<std::vector<G4double> > sources(30, std::vector<G4double>(4));
+
+sources[0][0] = 14.3;	
+sources[0][1] = 52.5;	
+sources[0][2] = 407.4;	
+sources[0][3] = 103.8;	
+  	
+sources[1][0] = 15.6;	
+sources[1][1] = 43.75;	
+sources[1][2] = 405.5;	
+sources[1][3] = 113.2;	
+  	
+sources[2][0] = 16.9;	
+sources[2][1] = 35.;	
+sources[2][2] = 403.5;	
+sources[2][3] = 122.6;	
+  	
+sources[3][0] = 18.2;	
+sources[3][1] = 26.25;	
+sources[3][2] = 401.3;	
+sources[3][3] = 131.9;	
+  	
+sources[4][0] = 19.5;	
+sources[4][1] = 17.5;	
+sources[4][2] = 398.8;	
+sources[4][3] = 141.2;	
+  	
+sources[5][0] = 20.8;	
+sources[5][1] = 8.75;	
+sources[5][2] = 396.1;	
+sources[5][3] = 150.5;	
+ 	
+sources[6][0] = 22.1;	
+sources[6][1] = 0.;	
+sources[6][2] = 393.2;	
+sources[6][3] = 159.7;	
+  	
+sources[7][0] = 23.4;	
+sources[7][1] = 52.5;	
+sources[7][2] = 390.;	
+sources[7][3] = 168.8;	
+
+sources[8][0] = 24.7;	
+sources[8][1] = 42.;	
+sources[8][2] = 386.7;	
+sources[8][3] = 177.9;	
+  	
+sources[9][0] = 26.;	
+sources[9][1] = 31.5;	
+sources[9][2] = 383.1;	
+sources[9][3] = 186.9;	
+  	
+sources[10][0] = 27.3;	
+sources[10][1] = 21.;	
+sources[10][2] = 379.4;	
+sources[10][3] = 195.8;	
+
+
+sources[11][0] = 28.6;	
+sources[11][1] = 10.5;	
+sources[11][2] = 375.4;	
+sources[11][3] = 204.7;	
+  	  	
+sources[12][0] = 29.9;	
+sources[12][1] = 0.;	
+sources[12][2] = 371.2;	
+sources[12][3] = 213.4;
+  	
+sources[13][0] = 31.2;	
+sources[13][1] = 52.5;	
+sources[13][2] = 366.7;	
+sources[13][3] = 222.1;	
+  	
+sources[14][0] = 32.5;	
+sources[14][1] = 42.;	
+sources[14][2] = 362.1;	
+sources[14][3] = 230.7;	
+  	
+sources[15][0] = 33.8;	
+sources[15][1] = 31.5;	
+sources[15][2] = 357.4;	
+sources[15][3] = 239.2;	
+ 	
+sources[16][0] = 35.1;	
+sources[16][1] = 21.;	
+sources[16][2] = 352.2;	
+sources[16][3] = 247.6;	
+  	
+sources[17][0] = 36.4;	
+sources[17][1] = 10.5;	
+sources[17][2] = 347.;	
+sources[17][3] = 255.8;	
+  	
+sources[18][0] = 37.7;	
+sources[18][1] = 0.;	
+sources[18][2] = 341.5;	
+sources[18][3] = 264.;	
+  	  	
+sources[19][0] = 39.;	
+sources[19][1] = 52.5;	
+sources[19][2] = 335.9;	
+sources[19][3] = 272.;	
+  	  	
+sources[20][0] = 40.3;	
+sources[20][1] = 42.;	
+sources[20][2] = 330.1;	
+sources[20][3] = 279.9;	
+ 	  	
+sources[21][0] = 41.6;	
+sources[21][1] = 31.5;	
+sources[21][2] = 324.;	
+sources[21][3] = 287.7;	
+  	  	
+sources[22][0] = 42.9;	
+sources[22][1] = 21.;	
+sources[22][2] = 317.8;	
+sources[22][3] = 295.3;	
+  	  	
+sources[23][0] = 44.2;	
+sources[23][1] = 10.5;	
+sources[23][2] = 311.4;	
+sources[23][3] = 302.8;	
+  	  	
+sources[24][0] = 45.5;	
+sources[24][1] = 0.;	
+sources[24][2] = 304.8;	
+sources[24][3] = 310.2;	
+  	  
+sources[25][0] = 46.8;	
+sources[25][1] = 52.;	
+sources[25][2] = 298.;	
+sources[25][3] = 317.4;	
+  	  	
+sources[26][0] = 48.1;	
+sources[26][1] = 40.;	
+sources[26][2] = 291.1;	
+sources[26][3] = 324.4;	
+  	  	
+sources[27][0] = 49.4;	
+sources[27][1] = 28.;	
+sources[27][2] = 283.9;	
+sources[27][3] = 331.3;	
+	  	  	
+sources[28][0] = 50.7;	
+sources[28][1] = 16.;	
+sources[28][2] = 276.6;	
+sources[28][3] = 338.;	
+  	  	
+sources[29][0] = 52.;	//# T
+sources[29][1] = 4.;	    //# Q
+sources[29][2] = 269.2;	//# R
+sources[29][3] = 344.5;	//# Z+30
+//#source coordinates end
 //#		for conv_point in conv_point_list:
 		for (G4int i = 0; i < 30; ++i) {
-/*
+
 		//### translation and rotation
-			x = math.sin( sources[i][1] *deg) * ( - (sources[i][2]-1.5 ) )
-			y = math.sqrt( ( sources[i][2]-1.5 )*(sources[i][2]-1.5 ) - x*x )
-			z = -sources[i][3]
-	
-			rotationX=G4Core.HepRotation()
-			rotationZ=G4Core.HepRotation()
-			rotationX.rotateX( (-90+sources[i][0])*deg )
-			rotationZ.rotateZ( -sources[i][1] *deg )
-			source_distance=math.sqrt(x*x+y*y+z*z)
-			new_point=Vector((x,y,z))
+			G4double x = std::sin( sources[i][1] *deg) * ( - (sources[i][2]-1.5 ) );
+			G4double y = std::sqrt( ( sources[i][2]-1.5 )*(sources[i][2]-1.5 ) - x*x );
+			G4double z = -sources[i][3];
+
+			G4RotationMatrix* rotationX = new G4RotationMatrix();
+			G4RotationMatrix* rotationZ = new G4RotationMatrix();
+			rotationX->rotateX( (-90+sources[i][0])*deg );
+			rotationZ->rotateZ( -sources[i][1] *deg );
+			G4double source_distance = std::sqrt(x*x+y*y+z*z);
+			G4ThreeVector new_point(x,y,z);
 			//#print "HAHO",x,y,z, x*x+y*y+z*z
-			gunAxis=Vector((-x/source_distance, -y/source_distance, -z/source_distance ))
-*/
+			G4ThreeVector gunAxis(-x/source_distance, -y/source_distance, -z/source_distance );
+
 		//#### rotation end
 			for (G4double thetaStep = thetaDeg; thetaStep < thetaDeg+deltatheta; thetaStep += 360) { //in range(thetaDeg, thetaDeg+deltatheta, 360):
 				/*
-				set_source(new_point, rotationX, rotationZ, thetaStep, gunAxis)				
-				//#randomize((189554476, 1193620722))
-				//#print "*****seeds: ", thetaStep, (mainVars.myGenerator.GetSeedWord(0), mainVars.myGenerator.GetSeedWord(1))
-				th=G4Support.RunAsync(n_events) #this is qthreaded!
-				th.join() #let it finish
+				set_source(new_point, rotationX, rotationZ, thetaStep, gunAxis);
+				//#randomize((189554476, 1193620722));
+				//#print "*****seeds: ", thetaStep, (mainVars.myGenerator.GetSeedWord(0), mainVars.myGenerator.GetSeedWord(1));
+				th=G4Support.RunAsync(n_events); //#this is qthreaded!
+				th.join(); #let it finish
 				//#print "*****final seeds: ", thetaStep,  get_seeds(mainVars)
 				*/
 			}
